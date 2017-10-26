1fe94c073ff8c5de8014cac4ff19cdfa57def220
viking-v2 <31208951+viking-v2@users.noreply.github.com>
2017-10-26 14:04:38 +0200

    Inserted data in tables

--- video.sql
+++ video.sql
@@ -65,18 +65,22 @@ PRIMARY KEY (studio_id, film_id));
 
 
 
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'John Williams', 75, '1942-06-12', 'Male');
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'Hans Zimmer', 40, '1977-06-11', 'Male');
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'Alan Syvlesti', 55, '1952-06-12', 'Male');
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'Ennio Morrecone', 75, '1942-06-12', 'Male');
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'Junkie XL', 35, '1982-06-12', 'Male');
-INSERT INTO film_composer(composer_id, name, age, date_of_birth, gender)
+INSERT INTO film_composer
 VALUES(composer_id, 'Jóhann Jóhannsson', 35, '1985-06-12', 'Male');
+INSERT INTO film_composer
+VALUES(composer_id, 'Ramin Djawadi', 40, '1970-06-12', 'Male');
+INSERT INTO film_composer
+VALUES(composer_id, 'Howard Shore', 61, '1952-06-12', 'Male');
 
 
 INSERT INTO actor 
@@ -117,9 +121,23 @@ VALUES(film_id, 'Deadpool', 2016, 18, 125, 5);
 INSERT INTO film
 VALUES(film_id, 'Arrival', 2016, 15, 150, 6);
 INSERT INTO film
-VALUES(film_id, 'Indiana Jones', 1992, 15, 125, 1);
+VALUES(film_id, 'Indiana Jones', 1985, 15, 125, 1);
 INSERT INTO film
 VALUES(film_id, 'Schindlers list', 1992, 15, 192, 1);
+INSERT INTO film
+VALUES(film_id, 'Se7en', 1992, 15, 192, 5);
+INSERT INTO film
+VALUES(film_id, 'Goodfellas', 1993, 18, 192, 3);
+INSERT INTO film
+VALUES(film_id, 'Iron Man', 2007, 12, 140, 7);
+INSERT INTO film
+VALUES(film_id, 'Lord Of The Rings', 2001, 12, 220, 8);
+INSERT INTO film
+VALUES(film_id, 'Wonder Woman', 2017, 12, 160, 2);
+
+
+INSERT INTO film_actor(actor_id, film_id)
+VALUES (1, 1), (2, 3), (3, 1), (4, 4), (5, 11), (6, 2), (7,5), (8,8), (9,12), (10, 12);
 
 
 INSERT INTO director
@@ -134,28 +152,41 @@ INSERT INTO director
 VALUES(director_id, 'Peter Jackson', 55, '1962-09-27', 'Male');
 INSERT INTO director
 VALUES(director_id, 'Steven Spielberg', 69, '1949-09-27', 'Male');
+INSERT INTO director
+VALUES(director_id, 'Denis Villeneuve', 46, '1960-10-19', 'Male');
+INSERT INTO director
+VALUES(director_id, 'Patty Jenkins', 50, '1956-01-18', 'Female');
+INSERT INTO director
+VALUES(director_id, 'Jon Favreau', 49, '1965-09-27', 'Male');
+
+INSERT INTO film_director(director_id, film_id)
+VALUES(1,1), (2, 2), (2,6), (3,11), (4,3), (5,14), (7, 8), (8,15), (9,13);
+
+INSERT INTO studio
+VALUES(studio_id, '20th Century Fox', '1946-08-10', 'USA', 'Rupert Murdoch');
+INSERT INTO studio
+VALUES(studio_id, 'Weinsten', '1960-08-10', 'USA', 'Bob Weinstein');
+INSERT INTO studio
+VALUES(studio_id, 'WingNut Films', '1995-08-10', 'New Zeland', 'Peter Jackson');
+INSERT INTO studio
+VALUES(studio_id, 'Marvel Comics', '1960-08-10', 'USA', 'Stan Lee');
+INSERT INTO studio
+VALUES(studio_id, 'Syncopy', '1946-08-10', 'USA', 'Christopher Nolan');
+
+INSERT INTO film_studio(studio_id, film_id)
+VALUES(1, 1), (2,2), (2,6), (5,3), (5,4), (5,5), (1,6), (1,7), (1,8), (1,9), (1, 10), (1, 11), (2, 12), (3,14), (4, 13);
 
 
 
 
-
-
-
-Select *
-from film;
-
-SELECT*
-FROM film_actor;
 
 SELECT *
-FROM film
-JOIN film_composer ON film.composer_id = film_composer.composer_id;
-
-
-SELECT director.name, film.title
 FROM director
 JOIN film_director ON director.director_id = film_director.director_id
 JOIN film ON film.film_id = film_director.film_id;
+
+SELECT *
+from actor;
 
 
 
